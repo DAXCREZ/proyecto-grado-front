806444aac03b0fb1022491daddc8c1e6d9a27a8f
DAXCREZ <ldiegobayona@unicesar.edu.co>
2021-06-05 13:35:19 -0500

    input colocados Falta darle estilos

--- src/presentacion/paginas/administrador/Registrarpaciente_Admin.jsx
+++ src/presentacion/paginas/administrador/Registrarpaciente_Admin.jsx
@@ -19,8 +19,101 @@ class RegistrarPacienteAdminPag extends Component {
                     </div>
 
                     <div className="FormularioPaciente">
-                        <h1>Datos</h1>
+                        <h1 className="Titulo__form">Datos</h1>
                         <form action="">
+
+                            <div className="Datos1">
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Identificacion</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Nombres</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Apelkidos</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Direccion</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Fecha de cumpleaños</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Barrio</label>
+                                    <input type="text"/>
+                                </div>
+                            </div>
+
+                            <div className="Datos2">
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Telefono</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Celular</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Correo</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Acudiente</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Relacion</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Celular del acudiente</label>
+                                    <input type="text"/>
+                                </div>
+                            </div>
+
+                            <div className="Datos3">
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Direccion del acuediente</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Convenio</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Número de licencia</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">EPS</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <div className="DatosFormulario">
+                                    <label htmlFor="">Tipo de usuario</label>
+                                    <input type="text"/>
+                                </div>
+
+                                <button>Guardar</button>
+                            </div>
 
                         </form>
 
