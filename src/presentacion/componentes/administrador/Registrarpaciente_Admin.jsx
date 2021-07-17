06211ffc1d21c4f7dd8b15270efe3dacf06bb6b1
DAXCREZ <ldiegobayona@unicesar.edu.co>
2021-07-16 19:11:24 -0500

    cambio en los inputs

--- src/presentacion/componentes/administrador/Registrarpaciente_Admin.jsx
+++ src/presentacion/componentes/administrador/Registrarpaciente_Admin.jsx
@@ -1,15 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Menuprincipal from "../MenuPrinciapl/Menuprincipal";
 import paciente from "../../../assets/medico.png";
 import "./Registrarpaciente_Admin.css"
-import ContactanosInput from "./InputsRegistrarAdmin/ContactanosInput";
-import AcudienteInput from "./InputsRegistrarAdmin/AcudienteInput";
-import EpsInput from "./InputsRegistrarAdmin/EpsInput";
-import PersonaInput from "./InputsRegistrarAdmin/PersonaInput";
+
+
 
 
 function RegistrarPacienteAdminPag () {
-    const [mostraInfo, setMostrarInfo] = useState(<PersonaInput/>);
+    const [personales, setPersonales] = useState("personales");
+    const [contacto, setContacto] = useState("contactoOff");
+    const [acudiente, setAcudiente] = useState("acudienteOff");
+    const [eps, setEps] = useState("epsOff");
     const [coloMenu1, setColorMenu1] = useState("Menu__datos-liActivo");
     const [coloMenu2, setColorMenu2] = useState("Menu__datos-li");
     const [coloMenu3, setColorMenu3] = useState("Menu__datos-li");
@@ -18,35 +19,49 @@ function RegistrarPacienteAdminPag () {
 
 
     function RenderizarInformacion (id){
+
         if (id === 1){
-            setMostrarInfo(<PersonaInput/>)
             setColorMenu1("Menu__datos-liActivo")
             setColorMenu2("Menu__datos-li")
             setColorMenu3("Menu__datos-li")
             setColorMenu4("Menu__datos-li")
             setBotonRegistrar("BotonRegistrarPaciente")
+            setPersonales("personales")
+            setContacto("contactoOff")
+            setAcudiente("acudienteOff")
+            setEps("epsOff")
         }else if(id === 2){
-            setMostrarInfo(<ContactanosInput/>)
             setColorMenu1("Menu__datos-li")
             setColorMenu2("Menu__datos-liActivo")
             setColorMenu3("Menu__datos-li")
             setColorMenu4("Menu__datos-li")
             setBotonRegistrar("BotonRegistrarPaciente")
+            setContacto("contacto")
+            setPersonales("personalesOff")
+            setAcudiente("acudienteOff")
+            setEps("epsOff")
         }else if(id === 3){
-            setMostrarInfo(<AcudienteInput/>)
             setColorMenu1("Menu__datos-li")
             setColorMenu2("Menu__datos-li")
             setColorMenu3("Menu__datos-liActivo")
             setColorMenu4("Menu__datos-li")
             setBotonRegistrar("BotonRegistrarPaciente")
+            setAcudiente("acudiente")
+            setPersonales("personalesOff")
+            setContacto("contactoOff")
+            setEps("epsOff")
         }else if(id === 4){
-            setMostrarInfo(<EpsInput/>)
             setColorMenu1("Menu__datos-li")
             setColorMenu2("Menu__datos-li")
             setColorMenu3("Menu__datos-li")
             setColorMenu4("Menu__datos-liActivo")
             setBotonRegistrar("BotonRegistrarPacienteActivo")
+            setPersonales("personalesOff")
+            setContacto("contactoOff")
+            setAcudiente("acudienteOff")
+            setEps("eps")
         }
+
     }
 
         return (
@@ -76,15 +91,110 @@ function RegistrarPacienteAdminPag () {
 
                                 <form className="form" action="">
                                     <div className="form__personales">
-                                        {
-                                            mostraInfo
-                                        }
+                                        <div className={personales}>
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="number" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Identificacion</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Nombres</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Apellidos</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Direccion</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Fecha de cumpleaños</label>
+                                            </div>
+                                        </div>
+
+                                        {/*Contactanos*/}
+
+                                        <div className={contacto}>
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Barrio</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Telefono</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Celular</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Correo</label>
+                                            </div>
+                                        </div>
+
+
+                                        {/*acudiente*/}
+                                        <div className={acudiente}>
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Nombre del Acudiente</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Relacion</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Celular del Acudiente</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Direccion del Acudiente</label>
+                                            </div>
+                                        </div>
+
+
+                                        {/*Eps*/}
+                                        <div className={eps}>
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Convenio</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Numero de licencia</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Eps</label>
+                                            </div>
+
+                                            <div className="paciente__grupo__form">
+                                                <input className="paciente__input" type="text" required/> <span className="barra"></span>
+                                                <label className="paciente__label" htmlFor="">Tipo de Usuario</label>
+                                            </div>
+                                        </div>
+
                                     </div>
 
                                     <button className={botonRegistrar}> Registrar </button>
 
                                 </form>
-
                             </div>
                         </div>
 
