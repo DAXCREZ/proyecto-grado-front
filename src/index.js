4c09a0b0659012234f554a2a8188f525e3e45349
DAXCREZ <lubafa2014@gmail.com>
2021-08-23 20:02:25 -0500

    Renovando proyecto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RegistrarPacienteAdminPag from "./presentacion/componentes/administrador/Registrarpaciente_Admin";
 
 ReactDOM.render(
   <React.StrictMode>
-    <RegistrarPacienteAdminPag/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
